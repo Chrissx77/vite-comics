af7a408cd8e9732e6d11b6c4cd976ca4468f5d6f
ChrisGarozzo <garozzochristian@gmail.com>
2023-12-21 22:24:39 +0100

    config scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+  name: "AppHeader",
+  data(){
+    return{
+
+    }
+  },
+}
+
+</script>
+
+<template>
+  <nav></nav>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/_variables.scss' as *;
+
+h1{
+  color: $primary;
+}
+</style>
