6f0ce9f0cd6f146c7130f9f78b44983bcbfb550a
ChrisGarozzo <garozzochristian@gmail.com>
2023-12-30 18:19:34 +0100

    added dinamic image with file json on vueJS

---
+++ src/components/AppMainProduct.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: "AppMainProduct",
+    props: {
+        details: Object,
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src=details.thumb alt="">
+        <small>
+            {{ details.series }}
+        </small>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+
+.card {    
+    flex-basis: 15%;
+    object-fit: contain;
+
+    img {
+        width: 100%;
+        height: 200px;
+        object-fit: cover;
+        object-position:top;
+    }
+}
+</style>
